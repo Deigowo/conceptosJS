bfba16e6f73aef42eb4e978a6734a5f209102aec
Diego Nicolas <diegonicolasriverasanchez@gmail.com>
2024-09-12 11:30:21 -0600

    03 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,40 @@
+//Operadores matematicos
+// + y - 
+// Operadores matemáticos
+
+console.log(10 + 3)
+console.log(10 - 3)
+console.log(10 * 3)
+console.log(10 / 3)
+console.log(10 % 3)
+console.log(10 ** 3)
+
+//Operadores string
+// concatenacion con +
+console.log("Hola" + " " + "Mundo")
+
+// operadores lógicos
+let a = 1
+a += 3
+a -= 3
+a *= 3
+a /= 3
+a %= 3
+a **= 3
+
+console.log(a)
+
+// Operadores de comparación
+console.log(10 > 3)
+console.log(10 < 3)
+console.log(10 >= 3)
+console.log(10 <= 3)
+console.log(10 == 3)
+console.log(10 != 3)
+console.log(10 === 3)
+console.log(10 !== 3)
+
+// Operadores Lógicos
+console.log(true && true) //And
+console.log(true || true) //Or
+console.log(!true) //Not
---
+++ concepts/variables.js
@@ -0,0 +1,19 @@
+//Variables en JavaScript
+//Declaramos variables con let o const
+//let para variables que cambiarán de valor
+//const para variables que no cambiarán de valor
+
+let name
+console.log(name)
+name = "Peter"
+console.log(name)
+console.log(typeof name)
+name = 19
+console.log(name)
+console.log(typeof name)
+
+const PI = 3.1416
+console.log(PI)
+console.log(typeof PI)
+// PI = 3.15 //Error
+
