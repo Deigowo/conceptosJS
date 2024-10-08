f211014ace21a393ec3dfa36658ff895b1976499
Diego Nicolas <diegonicolasriverasanchez@gmail.com>
2024-10-08 13:53:05 -0600

    13 actualizando dynamicdiv y todoapp

--- DOM/todoapp/app.js
+++ DOM/todoapp/app.js
@@ -1,11 +1,14 @@
 const inputTarea = document.getElementById('tareatxt')
 const btnAgregar = document.getElementById('addTarea')
 const divTareas = document.getElementById('tareas')
+const myForm = document.querySelector('#myForm')
+
 
 addTarea.addEventListener('click', () => {
     const tarea = inputTarea.value
     divTareas.appendChild(createTarea(tarea))
     inputTarea.value = ''
+    localStorage.setItem('miTarea', tarea)
     }
 )
 
@@ -23,11 +26,32 @@ const createTarea = (tarea) => {
     button.addEventListener('click', () => {
         div.remove()
     })
+
     p.innerText = tarea
     button.innerText = 'Eliminar'
-    // button.addEventListener('click', deleteItem)
     div.appendChild(p)
     div.append(button)
+    div.classList.add('alert')
+    div.classList.add('success')
+    div.addEventListener('click', changeColor)
     return div
-
 }
+const changeColor = (e) => {
+    const cDiv = e.target
+    if(cDiv.classList.contains('success')) {
+        cDiv.classList.remove('success')
+        cDiv.classList.add('warning')
+    } else if (cDiv.classList.contains('warning')){
+        cDiv.classList.remove('warning')
+        cDiv.classList.add('danger')
+    } else if (cDiv.classList.contains('danger')){
+        cDiv.classList.remove('danger')
+        cDiv.classList.add('success')
+    }
+}
+
+// btnAgregar.addEventListener('click', addTarea)
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTarea()
+})
--- DOM/todoapp/dynamicdiv/app.js
+++ DOM/todoapp/dynamicdiv/app.js
@@ -1,9 +1,13 @@
-const inputBorder = document.querySelector('#border')
+const inputBorderRadius = document.querySelector('#border')
 const inputWidth = document.querySelector('#width')
+const inputHeight = document.querySelector('#height')
+const inputShadowBox = document.querySelector('#shadowBox')
 const div = document.querySelector('.container')
 
-let border = 0
 let width = 0
+let borderRadius = 0
+let shadowBox = 0
+let height = 0
 
 const changeWidth = () => {
     return `${width}px`
@@ -13,21 +17,30 @@ const changeHeight = () => {
     return `${height}px`
 }
 
-const changeBorder = () => {
-    return `${border}px`
+const changeBorderRadius = () => {
+    return `${borderRadius}px`
 }
 
-inputBorder.addEventListener('change', (e) => {
-    border = e.target.value
-    div.style.borderRadius = changeBorder()
+const changeShadowBox = () => {
+    return `0px 0px ${shadowBox}px rgba(0, 0, 0, 0.5)`
+}
+
+inputBorderRadius.addEventListener('input', (e) => {
+    borderRadius = e.target.value
+    div.style.borderRadius = changeBorderRadius()
 })
 
-inputWidth.addEventListener('change', (e) => {
+inputWidth.addEventListener('input', (e) => {
     width = e.target.value
     div.style.width = changeWidth()
 })
 
-inputHeight.addEventListener('change', (e) => {
+inputHeight.addEventListener('input', (e) => {
     height = e.target.value
     div.style.height = changeHeight()
+})
+
+inputShadowBox.addEventListener('input', (e) => {
+    shadowBox = e.target.value
+    div.style.boxShadow = changeShadowBox()
 })
